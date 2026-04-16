408a99b77efccf6a1828bc6e08848a55b92d6f4e
Nathan Su <nlsu@ucsd.edu>
2026-04-15 21:52:54 -0700

    Update global.js
    fix resume

--- global.js
+++ global.js
@@ -9,7 +9,7 @@ let pages = [
   { url: 'index.html', title: 'Home' },
   { url: 'projects/index.html', title: 'Projects' },
   { url: 'contact/index.html', title: 'Contact' },
-  { url: 'resume/index.html', title: 'Resume' },
+  { url: 'Resume/index.html', title: 'Resume' },
   { url: 'https://github.com/NathanSu-afk', title: 'GitHub' },
 ];
 
