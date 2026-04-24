3af97e73ba304cc83cafaa2c3ca7567b31d5e95a
Nathan Su <nlsu@ucsd.edu>
2026-04-24 13:01:34 -0700

    Lab 4

--- global.js
+++ global.js
@@ -79,3 +79,28 @@ select.addEventListener('input', function (event) {
 if (localStorage.colorScheme) {
   setColorScheme(localStorage.colorScheme);
 }
+
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+    containerElement.innerHTML = '';
+
+    for (let project of projects) {
+        const article = document.createElement('article');
+
+        article.innerHTML = `
+            <${headingLevel}>${project.title}</${headingLevel}>
+            <img src="${project.image}" alt="${project.title}">
+            <p>${project.description}</p>
+        `;
+
+        containerElement.appendChild(article);
+    }
+}
